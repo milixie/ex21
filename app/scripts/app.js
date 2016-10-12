b0f8c68f8a5c1b5012efc4f64c9aa6504b212ca6
milixie <1435606568@qq.com>
2016-10-12 10:48:45 +0800

    section2 finished

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,51 +1,87 @@
-'use strict';
+$(function(){
+    $('#dowebok').fullpage({
+      menu: '#menu',
+      lockAnchors: false,
+      anchors:['info', 'show', 'exper', 'about'],
+      navigation: true,
+      navigationPosition: 'right',
+      navigationTooltips: ['自我描述', '作品展示', '项目经验', '关于'],
+      showActiveTooltip: false,
+      slidesNavigation: true,
+      slidesNavPosition: 'bottom',
 
-/**
- * @ngdoc overview
- * @name projectNameApp
- * @description
- * # projectNameApp
- *
- * Main module of the application.
- */
-angular
-  .module('projectNameApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'fullPage.js'
-  ])
-  .config(function ( $routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/show', {
-        templateUrl: 'views/show.html',
-        controller: 'ShowCtrl',
-        controllerAs: 'show'
-      })
-      .when('/exper', {
-        templateUrl: 'views/exper.html',
-        controller: 'ExperCtrl',
-        controllerAs: 'exper'
-      })
-      .when('/info', {
-        templateUrl: 'views/info.html',
-        controller: 'InfoCtrl',
-        controllerAs: 'info'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+      //Scrolling
+      css3: true,
+      scrollingSpeed: 700,
+      autoScrolling: true,
+      fitToSection: true,
+      fitToSectionDelay: 1000,
+      scrollBar: false,
+      easing: 'easeInOutCubic',
+      easingcss3: 'ease',
+      loopBottom: false,
+      loopTop: false,
+      loopHorizontal: true,
+      continuousVertical: false,
+      continuousHorizontal: false,
+      scrollHorizontally: false,
+      interlockedSlides: false,
+      resetSliders: false,
+      fadingEffect: false,
+      normalScrollElements: '#element1, .element2',
+      scrollOverflow: false,
+      scrollOverflowOptions: null,
+      touchSensitivity: 15,
+      normalScrollElementTouchThreshold: 5,
+      bigSectionsDestination: null,
+
+      //Accessibility
+      keyboardScrolling: true,
+      animateAnchor: true,
+      recordHistory: true,
+
+      //Design
+      controlArrows: true,
+      verticalCentered: true,
+      sectionsColor : ['#ccc', '#fff'],
+      paddingTop: '3em',
+      paddingBottom: '10px',
+      fixedElements: '#header, .footer',
+      responsiveWidth: 0,
+      responsiveHeight: 0,
+      responsiveSlides: false,
+
+      //Custom selectors
+      sectionSelector: '.section',
+      slideSelector: '.slide',
+
+      //events
+      onLeave: function(index, nextIndex, direction){},
+      afterLoad: function(anchorLink, index){},
+      afterRender: function(){},
+      afterResize: function(){},
+      afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+      onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+    });
+
+    $(window).resize(function(){
+        autoScrolling();
+    });
+
+    function autoScrolling(){
+        var $ww = $(window).width();
+        if($ww < 1024){
+            $.fn.fullpage.setAutoScrolling(false);
+        } else {
+            $.fn.fullpage.setAutoScrolling(true);
+        }
+    }
+
+    autoScrolling();
+
+    $('.work-img img').on('click', function(){
+      $(this).parent('.work-img').parent('.work-items').addClass('activeShow').siblings('.work-items').removeClass('activeShow');
+      $('.activeShow').siblings('.work-items').children('.work-detail').slideUp();
+      $(this).parent('.work-img').siblings('.work-detail').slideToggle();
+    });
+});
